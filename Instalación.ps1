8bdbd2d8bb9288662ca6f5011da7615e4110e544
Javier Garzon <9005612@alumnos.ufv.es>
2024-06-27 01:51:32 +0200

    otro comit

--- Instalación.ps1
+++
@@ -1,6 +0,0 @@
-﻿Start-Process powershell -ArgumentList "-NoExit", "-Command", {
-    python -m venv venv;
-    .\venv\Scripts\activate;
-    pip install --timeout 300 -r requirements.txt;
-    Read-Host -Prompt 'Instalación terminada pulsa enter para continuar'
-}
